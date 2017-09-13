fc9ceb4cbae5f56d3db951e23f1fcfbb11c94333
Annoying Jorange <jorsanders@hotmail.com>
2017-09-13 14:41:06 +0200

    fixed php build. now we display php errors

--- src/index.php
+++ src/index.php
@@ -1,16 +1,15 @@
 <?php
 
 // Test mysqli connection
-$link = mysqli_connect("192.168.0.7", "dbuser", "123", "dbname");
+$link = true;
+$link = mysqli_connect("192.168.0.7", "unnamedproject", "123", "unnamedproject");
 
+var_dump($link);
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
 }
-
-var_dump($link);
 
 
 phpinfo();
