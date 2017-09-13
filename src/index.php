af8185b56d98439c930e33433db5263dea90562a
Annoying Jorange <jorsanders@hotmail.com>
2017-09-13 14:52:23 +0200

    cleaned up unnessairy stuff

--- src/index.php
+++ src/index.php
@@ -1,11 +1,10 @@
 <?php
 
 // Test mysqli connection
-$link = true;
-$link = mysqli_connect("192.168.0.7", "unnamedproject", "123", "unnamedproject");
+$db_connection = mysqli_connect("192.168.0.7", "unnamedproject", "123", "unnamedproject");
 
-var_dump($link);
-if (!$link) {
+var_dump($db_connection);
+if (!$db_connection) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
