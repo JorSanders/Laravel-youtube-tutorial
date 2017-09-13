659c73b63103c30e3a29a4f5f9a575a23f133bc1
Annoying Jorange <jorsanders@hotmail.com>
2017-09-13 11:56:20 +0200

    Added mysql database and nginx logs

--- src/index.php
+++ src/index.php
@@ -1,6 +1,16 @@
 <?php
 
-//Just declared this variable to test xdebug
-$uselessVar = true;
+// Test mysqli connection
+$link = mysqli_connect("192.168.0.7", "dbuser", "123", "dbname");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+var_dump($link);
+
 
 phpinfo();
