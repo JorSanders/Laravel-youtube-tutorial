66f6a66066e3e6cc1e8f231ff09854d4f0103a52
Annoying Jorange <jorsanders@hotmail.com>
2018-04-23 17:28:22 +0800

    Redid make:auth

--- lara-yout/routes/web.php
+++ lara-yout/routes/web.php
@@ -30,3 +30,23 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
